4898328b2c440dfb9bbdb28152cba12965b38151
Paul <estudio26h@gmail.com>
2020-06-14 22:34:38 +0200

    CSS Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,8 @@ jQuery(document).ready(function() {
     });
 });
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
